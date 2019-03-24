37f8cf84eae228daa3d1ca47e98db73b1b2d4666
Rainer Paat <rapaat@gmail.com>
2019-03-24 19:21:03 +0200

    Signaling is in place

--- rcsnail/rcs_livesession.py
+++ rcsnail/rcs_livesession.py
@@ -21,19 +21,29 @@ from aiortc import (RTCIceCandidate, RTCPeerConnection, RTCSessionDescription,
                     VideoStreamTrack)
 from aiortc.contrib.media import MediaBlackhole, MediaPlayer, MediaRecorder
 from aiortc.contrib.signaling import object_from_string, object_to_string
+from aiortc.sdp import candidate_from_sdp, candidate_to_sdp
 
 ROOT = os.path.dirname(__file__)
 PHOTO_PATH = os.path.join(ROOT, 'photo.jpg')
 
 class RCSSignaling:
-    def __init__(self, auth, rs_url, post_url):
+    def __init__(self, auth, rs_url, post_url, loop):
         self.__auth = auth
         self.__rs_url = rs_url
         self.__post_url = post_url
         self.__message_queue = asyncio.Queue()
+        self.__uid = auth.current_user['localId']
+        self.__loop = loop
 
     def rs_error(self):
         print("error")
+
+    def handle_message(self, key, data):
+        if key in self.__received_messages:
+            return
+        self.__received_messages[key] = data
+        # await self.__message_queue.put(data)
+        asyncio.run_coroutine_threadsafe(self.__message_queue.put(data), loop = self.__loop)
 
     def rs_message(self, message):
         print(message)
@@ -41,33 +51,25 @@ class RCSSignaling:
             data = json.loads(message.data)
             data_path = data['path']
             data_data = data['data']
-            queue_message = None
             if data_path == '/':
-                if 'messages' in data_data:
+                if data_data != None and 'messages' in data_data:
                     # parse existing messages
-                    pass
-                    # rs_url = data_data['rsUrl']
-            elif data_path == '/messages':
-                # data_path for new message '/messages/-LaP0ffzKHctPD1uQs7M'
-                # parse new messages when data_data['uid'] == uid
+                    data_data_messages = data_data['messages']
+                    for message in data_data_messages:
+                        self.handle_message('/' + message, data_data_messages[message])
+            else:
+                # data_path for new message '/-LaP0ffzKHctPD1uQs7M'
                 # data_data['type']
                 # data_data['sdp'] or data_data['candidate']
-                pass
-                # rs_url = data_data
-                
-                # parse remote session info
-                # full record or change in root
-
-            if queue_message != None:
-                self.__message_queue.put(queue_message)
-            #asyncio.run_coroutine_threadsafe(event_queue.put(event), loop=loop)
-            print('Remote session message')
+                self.handle_message(data_path, data_data)
 
     async def rs_listen(self):
         async for event in self.__event_source:
-            print(event)
+            pass
+            #print(event)
 
     async def connect(self):
+        self.__received_messages = {} # empty dict for received messages
         headers = {
             "Authorization": "Bearer " + self.__auth.current_user['idToken'],
             'content-type': 'application/json'
@@ -84,11 +86,7 @@ class RCSSignaling:
         )
         await self.__event_source.connect()
         self.__rs_task = asyncio.ensure_future(self.rs_listen())
-        # self.__queue_task = asyncio.ensure_future(self.queue_listen())
 
-        return
-        # self.__messages = params['messages']
-        # self.__post_url = params['postUrl']
 
     async def close(self):
         if self.__event_source:
@@ -102,7 +100,21 @@ class RCSSignaling:
         message = await self.__message_queue.get()
         # message = json.loads(message)['msg']
         print('<', message)
-        return object_from_string(message)
+        # msg = json.dumps(message)
+        if message['type'] in ['answer', 'offer']:
+            msg = {
+                'sdp': message['sdp'],
+                'type': message['type']
+            }
+            return RTCSessionDescription(**msg)
+        elif message['type'] == 'candidate':
+            candidate = candidate_from_sdp(message['candidate'])
+            if 'sdpMid' in message:
+                candidate.sdpMid = message['sdpMid']
+            candidate.sdpMLineIndex = message['sdpMLine']
+            return candidate
+        else:
+            return None
 
     async def send(self, obj):
         message = object_to_string(obj)
@@ -147,38 +159,11 @@ class RCSLiveSession(object):
         self.__auth = auth
         self.__queueUrl = queueUrl
         self.__loop = loop
+        self.__uid = auth.current_user['localId']
         print('queueUrl ' + queueUrl)
-        self.__db = self.__firebase_app.database()
-        self.__fb_queue = asyncio.Queue()
-        # self.__queue_stream = self.__db.child(queuePath).stream(self.queue_stream_handler, token = self.__auth.current_user['idToken'])
-        # self.__queue_live = LiveData(self.__firebase_app, queuePath)
-        # self.__queue_live.signal('/').connect(self.queue_handler)
-
-    def create_remote_session(self, rsUrl):
-        '''
-        pc = RTCPeerConnection()
-        self.__pc = pc
-
-        @pc.on('track')
-        def on_track(track):
-            print('Track %s received' % track.kind)
-            # recorder.addTrack(track)
-        rsPath = '/rs/test'
-        class Track(object):
-            pass
-        track = Track()
-        track.kind = "test"
-        pc.emit('track', track)
-        '''
-        rsPath = '/rs/test'
-        # self.__rs_stream = self.__db.child(rsPath).stream(self.rs_stream_handler, token = self.__auth.current_user['idToken'])
-        
 
     def close(self):
-        pass
-        if self.__rs_stream:
-            self.__rs_stream.close()
-            self.__rs_stream = None
+        print('Closing RCS live session')
 
     async def run_session(self, pc, player, recorder, signaling):
         def add_tracks():
@@ -200,8 +185,10 @@ class RCSLiveSession(object):
 
         # send offer
         add_tracks()
-        await pc.setLocalDescription(await pc.createOffer())
+        offer = await pc.createOffer()
+        await pc.setLocalDescription(offer)
         await signaling.send(pc.localDescription)
+        # await signaling.send(offer)
         print('Offer sent')
 
         # consume signaling
@@ -211,15 +198,14 @@ class RCSLiveSession(object):
             if isinstance(obj, RTCSessionDescription):
                 await pc.setRemoteDescription(obj)
                 await recorder.start()
-
-                if obj.type == 'offer':
-                    print('Got wrong offer. Exiting')
-                    break
             elif isinstance(obj, RTCIceCandidate):
-                pc.addIceCandidate(obj)
+                    pc.addIceCandidate(obj)
             else:
                 print('Exiting')
                 break
+
+        await signaling.close()
+
 
     def new_frame(self, frame):
         print('Received new frame')
@@ -238,7 +224,7 @@ class RCSLiveSession(object):
             try:
                 async for event in event_source:
                     print(event)
-                    if event.message == 'put':
+                    if event.message == 'put' or event.message == 'patch':
                         data = json.loads(event.data)
                         data_path = data['path']
                         data_data = data['data']
@@ -257,8 +243,6 @@ class RCSLiveSession(object):
         return rs_url, rs_post_url
 
     async def run(self, new_frame_callback):
-        # start listening queue item
-        # self.__queue_stream = self.__db.child(queuePath).stream(self.queue_stream_handler, token = self.__auth.current_user['idToken'])
         self.__new_frame_callback = new_frame_callback
 
         # wait for queue to return remote session url
@@ -267,7 +251,7 @@ class RCSLiveSession(object):
             return
 
         # create signaling and peer connection
-        signaling = RCSSignaling(self.__auth, rs_url, rs_post_url)
+        signaling = RCSSignaling(self.__auth, rs_url, rs_post_url, self.__loop)
         pc = RTCPeerConnection()
 
         # create media source
@@ -291,6 +275,6 @@ class RCSLiveSession(object):
             # cleanup
             await recorder.stop()
             await signaling.close()
-            await pc.close()
+            # await pc.close()
 
 
